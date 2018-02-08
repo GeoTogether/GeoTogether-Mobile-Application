aa238e2cd775ad981dcc65c9ab7bd772fa722c30
Mahdi Alharbi <malharb3@asu.edu>
2018-02-08 02:14:19 -0700

    Making the invite view for to allow invitations to be sent. Have to work on layouts and email invite with addition of buttons.

---
+++ src/components/InviteScreen/InviteView.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+export default class InviteView extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+    }
+
+    var SendIntentAndroid = require('react-native-send-intent');
+
+    onPressTextMessage(){
+        SendIntentAndroid.sendText({
+            title: 'Title of the text message',
+            text: 'The actual message goes here',
+            type: SendIntentAndroid.TEXT_PLAIN
+        });
+    }
+
+
+    renderCurrentState() {
+
+        const { navigate } = this.props.navigation;
+
+        return (
+
+            <LinearGradient colors={['#00B4AB', '#FE7C00']} style={styles.linearGradient}>
+
+            </LinearGradient>
+
+        );
+
+    }
+
+
+    render() {
+        const { navigate } = this.props.navigation;
+
+        return (
+            <KeyboardAvoidingView behavior="padding" style={styles.container}>
+
+                <View style={styles.Container}>
+
+
+                    <TouchableOpacity onPress={() => this.onPressTextMessage()} style={styles.buttonContainer} >
+                        <Text style={styles.buttonText}>Log Out</Text>
+                    </TouchableOpacity>
+
+                    //Need another button for email invite!
+
+                </View>
+            </KeyboardAvoidingView>
+        )
+
+    }
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
