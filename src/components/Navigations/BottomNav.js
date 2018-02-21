fb1e7b613eba47886f125485b2822e6ec03e7415
kjvande3 <kjvande3@asu.edu>
2018-02-20 20:15:09 -0700

    finished styling bottom nav bar

--- src/components/Navigations/BottomNav.js
+++ src/components/Navigations/BottomNav.js
@@ -1,44 +1,51 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation';
+import { View, StyleSheet, TouchableOpacity, Text, Image } from 'react-native';
 
 export default class BottomNav extends Component {
     render() {
         return (
-            <View style={{ flex: 1 }}>
-                <BottomNavigation
-                    labelColor="white"
-                    rippleColor="white"
-                    style={styles.bottomNavigation}
-                >
-                    <Tab
-                        barBackgroundColor="#37474F"
-                        label="Movies & TV"
+            <View style={styles.containerNav}>
+
+                <TouchableOpacity style={styles.navButtonContainer}>
+                    <Image
+                        style={styles.navIcons}
+                        source={require('../../images/chat.png')}
                     />
-                    <Tab
-                        barBackgroundColor="#00796B"
-                        label="Music"
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.navButtonContainer}>
+                    <Image
+                        style={styles.navIcons}
+                        source={require('../../images/home.png')}
                     />
-                    <Tab
-                        barBackgroundColor="#5D4037"
-                        label="Books"
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.navButtonContainer}>
+                    <Image
+                        style={styles.navIcons}
+                        source={require('../../images/upload.png')}
                     />
-                    <Tab
-                        barBackgroundColor="#3E2723"
-                        label="Newsstand"
-                    />
-                </BottomNavigation>
+                </TouchableOpacity>
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    bottomNavigation: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
-        height: 56
+    containerNav:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+    },
+    navButtonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFF',
+        padding: '3%',
+        width: '33.33333%',
+    },
+    navIcons: {
+        width: 25,
+        height: 25
     }
 })
