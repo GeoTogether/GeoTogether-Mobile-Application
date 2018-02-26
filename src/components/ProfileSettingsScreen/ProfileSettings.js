fb618a6342b4f770bd362e7f497691c7cde5233e
Mahdi Alharbi <malharb3@asu.edu>
2018-02-25 23:46:56 -0700

    Made some progress towards responsiveness, still needs some work and some changes in the looks as well.

--- src/components/ProfileSettingsScreen/ProfileSettings.js
+++ src/components/ProfileSettingsScreen/ProfileSettings.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, PixelRatio, TouchableOpacity, StyleSheet, Image, View } from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, Text, TextInput, PixelRatio, TouchableOpacity, StyleSheet, Image, View} from 'react-native';
 
 import firebase from '../Firebase/firebaseStorage';
 import ImagePicker from 'react-native-image-picker';
@@ -8,18 +8,12 @@ import ImagePicker from 'react-native-image-picker';
 export default class ProfileSettings extends Component {
 
 
-
-    constructor(props) {
-        super(props)
-    }
-
-    // navigation options to be used to navigate the class from other classes
-
     static navigationOptions = {
         title: 'ProfileSettings',
         header: null
     }
 
+    // navigation options to be used to navigate the class from other classes
     state = {
         email: '',
         photo: '',
@@ -29,102 +23,105 @@ export default class ProfileSettings extends Component {
         photoS: null,
     }
 
-
-    componentWillMount() {
-
-        const { state } = this.props.navigation;
-        this.setState({ email: state.params.email });
-
-
-        // get all the users from the firebase database
-        firebase.database().ref("users").on('value', (snapshot) => {
-            snapshot.forEach((userSnapshot) => {
-
-
-                const val = userSnapshot.val();
-
-
-                if (val.email == this.state.email) {
-
-
-                    this.setState({ fname: val.first });
-                    this.setState({ lname: val.last });
-                    this.setState({ photo: val.photo })
-
-
-
-
-                    if (this.state.photo == '') {
-
-
-                       // this.setState({ photoS: require('../../images/face.png') })
-
-                    } else {
-
-                        this.setState({ photoS: { uri: this.state.photo } })
-                    }
-
-
-
-                }
-
-            })
-        })
-
-
-
+    constructor(props) {
+        super(props)
     }
 
 
-
-    componentDidMount() {
-        const { state } = this.props.navigation;
-        this.setState({ email: state.params.email });
-        // get all the users from the firebase database
-        // get all the users from the firebase database
-        firebase.database().ref("users").on('value', (snapshot) => {
-            snapshot.forEach((userSnapshot) => {
-
-
-                const val = userSnapshot.val();
-
-
-                if (val.email == this.state.email) {
-
-
-                    this.setState({ fname: val.first });
-                    this.setState({ lname: val.last });
-                    this.setState({ photo: val.photo })
-
-
-
-
-        if (this.state.photo == '') {
-            
-            
-                        //this.setState({ photoS: require('../../images/face.png') })
-            
-                    } else {
-
-                        this.setState({ photoS: { uri: this.state.photo } })
-                    }
-            
-
-
-                }
-
-            })
-        })
-
-
-
-
-
-
-
-
-    }
-
+    // componentWillMount() {
+    //
+    //     const { state } = this.props.navigation;
+    //     this.setState({ email: state.params.email });
+    //
+    //
+    //     // get all the users from the firebase database
+    //     firebase.database().ref("users").on('value', (snapshot) => {
+    //         snapshot.forEach((userSnapshot) => {
+    //
+    //
+    //             const val = userSnapshot.val();
+    //
+    //
+    //             if (val.email == this.state.email) {
+    //
+    //
+    //                 this.setState({ fname: val.first });
+    //                 this.setState({ lname: val.last });
+    //                 this.setState({ photo: val.photo })
+    //
+    //
+    //
+    //
+    //                 if (this.state.photo == '') {
+    //
+    //
+    //                    // this.setState({ photoS: require('../../images/face.png') })
+    //
+    //                 } else {
+    //
+    //                     this.setState({ photoS: { uri: this.state.photo } })
+    //                 }
+    //
+    //
+    //
+    //             }
+    //
+    //         })
+    //     })
+    //
+    //
+    //
+    // }
+    //
+    //
+    //
+    // componentDidMount() {
+    //     const { state } = this.props.navigation;
+    //     this.setState({ email: state.params.email });
+    //     // get all the users from the firebase database
+    //     // get all the users from the firebase database
+    //     firebase.database().ref("users").on('value', (snapshot) => {
+    //         snapshot.forEach((userSnapshot) => {
+    //
+    //
+    //             const val = userSnapshot.val();
+    //
+    //
+    //             if (val.email == this.state.email) {
+    //
+    //
+    //                 this.setState({ fname: val.first });
+    //                 this.setState({ lname: val.last });
+    //                 this.setState({ photo: val.photo })
+    //
+    //
+    //
+    //
+    //     if (this.state.photo == '') {
+    //
+    //
+    //                     //this.setState({ photoS: require('../../images/face.png') })
+    //
+    //                 } else {
+    //
+    //                     this.setState({ photoS: { uri: this.state.photo } })
+    //                 }
+    //
+    //
+    //
+    //             }
+    //
+    //         })
+    //     })
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // }
 
     selectPhotoTapped() {
         const options = {
@@ -149,7 +146,7 @@ export default class ProfileSettings extends Component {
                 console.log('User tapped custom button: ', response.customButton);
             }
             else {
-                let source = { uri: response.uri };
+                let source = {uri: response.uri};
 
                 // You can also display the image using data:
                 // let source = { uri: 'data:image/jpeg;base64,' + response.data };
@@ -164,44 +161,65 @@ export default class ProfileSettings extends Component {
     }
 
 
-
     render() {
 
         return (
-            <View>
-                {this.state.photoS == null ? (
-                    <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)} style={{justifyContent: 'center', alignItems: 'center', marginTop: 15}}>
+            <View style={styles.container}>
+                <View style={styles.imageContainer}>
+                    {this.state.photoS == null ? (
+                        <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
 
-                        <View style={styles.ImageContainer}>
+                            <View style={styles.imageComponent}>
 
-                            {
-                                this.state.ImageSource === null ? <Text>Select a Photo</Text> :
-                                <Image style={styles.ImageContainer} source={this.state.ImageSource} />
-                            }
+                                {
+                                    this.state.ImageSource === null ?
+                                        <Text style={{color: 'blue'}}>Select a Photo</Text> :
+                                        <Image source={this.state.ImageSource}/>
+                                }
 
-                        </View>
+                            </View>
 
-                    </TouchableOpacity>
-                ) : (
-                    <Image
-                        style={{ width: 100, height: 100, marginLeft: 155 }}
-                        source={this.state.photoS}
-                    />
-                )}
+                        </TouchableOpacity>
+                    ) : (
+                        <Image
+                            style={{width: 100, height: 100, marginLeft: 155}}
+                            source={this.state.photoS}
+                        />
+                    )}
+                </View>
 
 
-                <Text style={styles.labels}>Name</Text>
-                <TextInput style={styles.input}>
-                    {this.state.fname} {this.state.lname}
-                </TextInput>
-                <Text style={styles.labels}>Username</Text>
-                <TextInput style={styles.input}>
-                    {this.state.fname}
-                </TextInput>
-                <Text style={styles.labels}>Email</Text>
-                <TextInput style={styles.input2}>
-                    {this.state.email}
-                </TextInput>
+                <View style={styles.profileInfoContainer}>
+
+                    <View style={styles.displayHeader}>
+                    <Text style={styles.labels}>Name</Text>
+                    </View>
+                    <TextInput style={styles.input}>
+                        {this.state.fname} {this.state.lname}
+                    </TextInput>
+
+                    <View style={styles.displayHeader}>
+                    <Text style={styles.labels}>Username</Text>
+                    </View>
+                    <TextInput style={styles.input}>
+                        {this.state.fname}
+                    </TextInput>
+
+                    <View style={styles.displayHeader}>
+                    <Text style={styles.labels}>Email</Text>
+                    </View>
+                    <TextInput style={styles.input2}>
+                        {this.state.email}
+                    </TextInput>
+
+                    <View style={styles.displayHeader}>
+                    <Text style={styles.labels}>Gender</Text>
+                    </View>
+                    <TextInput style={styles.input2}>
+                        {this.state.email}
+                    </TextInput>
+
+                </View>
 
             </View>
 
@@ -209,52 +227,22 @@ export default class ProfileSettings extends Component {
         );
 
 
-
     }
 }
 
 const styles = StyleSheet.create({
-
-    toolbar: {
-        height: 50,
-        backgroundColor: 'white',
-    },
-    image: {
-        marginLeft: 160,
-
-
-    },
-    change: {
-        color: 'blue',
-        textAlign: 'center',
-    },
-    input: {
-        marginRight: 260,
-
-    },
-    input2: {
-        marginRight: 200,
-    },
-    labels: {
-        color: 'grey',
-        marginLeft: 20,
-    },
     container: {
         flex: 1,
-        marginTop: 15,
-        backgroundColor: '#F5FCFF',
+        backgroundColor: 'white',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    imageContainer: {
+        //backgroundColor: 'black',
+        flex: 2,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'center'
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-    ImageContainer: {
+    imageComponent: {
         borderRadius: 75,
         width: 150,
         height: 150,
@@ -263,6 +251,19 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'transparent',
-
     },
+    profileInfoContainer: {
+        flex: 3,
+        //height: '50%',
+        width: '80%',
+        justifyContent: 'space-between',
+        paddingLeft: 40,
+        paddingBottom: 100,
+    },
+    displayHeader:{
+        paddingTop: 15
+    },
+    labels:{
+        fontSize: 15
+    }
 });
