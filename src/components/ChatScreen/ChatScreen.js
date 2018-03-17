5a3b0a4afb908b27c64aa58837e8a11c754d075d
Benjamin <buriart2@asu.edu>
2018-03-17 12:58:01 -0700

    Started working on the chat screen ui using the package

---
+++ src/components/ChatScreen/ChatScreen.js
@@ -0,0 +1,48 @@
+import { GiftedChat } from 'react-native-gifted-chat';
+import React from 'react';
+import { View } from 'react-native';
+
+
+class ChatScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {messages: []};
+        this.onSend = this.onSend.bind(this);
+    }
+    componentWillMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://facebook.github.io/react/img/logo_og.png',
+                    },
+                },
+            ],
+        });
+    }
+    onSend(messages = []) {
+        this.setState((previousState) => {
+            return {
+                messages: GiftedChat.append(previousState.messages, messages),
+            };
+        });
+    }
+    render() {
+        return (
+            <View>
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={this.onSend}
+                user={{
+                    _id: 1,
+                }}
+            />
+            </View>
+        );
+    }
+}
