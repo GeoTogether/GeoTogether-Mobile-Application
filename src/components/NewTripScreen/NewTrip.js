29395f4126d8cec8b62b94e7a9bafb1f45c9d197
khalqaht <khalqaht@asu.edu>
2018-03-23 20:31:38 -0700

    samll fix for the new trip
    samll fix

--- src/components/NewTripScreen/NewTrip.js
+++ src/components/NewTripScreen/NewTrip.js
@@ -247,197 +247,196 @@ export default class NewTrip extends React.Component {
                             language: 'en', // language of the results
                         }}
                         onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-                       
-                        this.setState({ destination1: data.description })
-                    }}
-                    />
-                </View>
 
-
-            
-
-                    <View style={styles.addContiner}>
-                    <View style={styles.addFuncContainer}>
-
-                <View style={styles.endLocationContainer}>
-                    <Text style={styles.textHeader}>End Location</Text>
-                    <GooglePlacesAutocomplete
-                        placeholder='ex. Tempe, AZ'
-                        minLength={2}
-                        autoFocus={false}
-                        returnKeyType={'default'}
-                        fetchDetails={true}
-                        styles={{
-                            textInputContainer: {
-                                backgroundColor: 'rgba(0,0,0,0)',
-                                borderTopWidth: 0,
-                                borderBottomWidth: 0
-                            },
-                            textInput: {
-                                marginLeft: 0,
-                                marginRight: 0,
-                                height: 38,
-                                color: '#5d5d5d',
-                                fontSize: 16
-                            },
-                            predefinedPlacesDescription: {
-                                color: '#1faadb'
-                            },
-                        }}
-                        currentLocation={false}
-                        query={{
-                            key: ' AIzaSyAUdubBvZ7sDgU2ye17YHpuJo-OPjM4EzE',
-                            language: 'en', // language of the results
-                        }}
-                        onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-                          
-                            this.setState({ destination2: data.description })
+                            this.setState({ destination1: data.description })
                         }}
                     />
                 </View>
 
-                <View style={styles.addFuncContainer}>
 
-                    <TouchableOpacity onPress={() => this.openEvent()}>
-                        <Text style={styles.splitText}>+ ADD EVENT</Text>
-                    </TouchableOpacity>
 
-                    <TouchableOpacity onPress={() => this.openModal()}>
-                        <Text style={styles.splitText}>+ ADD MEMBERS</Text>
-                    </TouchableOpacity>
-                </View>
 
-                <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-                    <View style={styles.container2}>
+             
 
-                           <Modal
-                        visible={this.state.modalVisible}
-                        animationType={'slide'}
-                        onRequestClose={() => this.closeModal()}>
+                        <View style={styles.endLocationContainer}>
+                            <Text style={styles.textHeader}>End Location</Text>
+                            <GooglePlacesAutocomplete
+                                placeholder='ex. Tempe, AZ'
+                                minLength={2}
+                                autoFocus={false}
+                                returnKeyType={'default'}
+                                fetchDetails={true}
+                                styles={{
+                                    textInputContainer: {
+                                        backgroundColor: 'rgba(0,0,0,0)',
+                                        borderTopWidth: 0,
+                                        borderBottomWidth: 0
+                                    },
+                                    textInput: {
+                                        marginLeft: 0,
+                                        marginRight: 0,
+                                        height: 38,
+                                        color: '#5d5d5d',
+                                        fontSize: 16
+                                    },
+                                    predefinedPlacesDescription: {
+                                        color: '#1faadb'
+                                    },
+                                }}
+                                currentLocation={false}
+                                query={{
+                                    key: ' AIzaSyAUdubBvZ7sDgU2ye17YHpuJo-OPjM4EzE',
+                                    language: 'en', // language of the results
+                                }}
+                                onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
 
-                        <View style={styles.modalContainer}>
-                            <View style={styles.innerContainer}>
-
-                                <TouchableOpacity  onPress={() => this.sendSMS()}>
-                                    <Image source={require('../../images/sms.png')} />
-                                </TouchableOpacity>
-
-                                <TouchableOpacity
-                                    onPress={() => this.sendEmail()}>
-                                    <Image source={require('../../images/email.png')} />
-                                </TouchableOpacity>
-
-                                <TouchableOpacity  onPress={() => this.closeModal()} >
-                                    <Image source={require('../../images/cancel.png')} />
-                                </TouchableOpacity>
-                            </View>
+                                    this.setState({ destination2: data.description })
+                                }}
+                            />
                         </View>
-                    </Modal>
+
+                        <View style={styles.addFuncContainer}>
+
+                            <TouchableOpacity onPress={() => this.openEvent()}>
+                                <Text style={styles.splitText}>+ ADD EVENT</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity onPress={() => this.openModal()}>
+                                <Text style={styles.splitText}>+ ADD MEMBERS</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+                            <View style={styles.container2}>
+
+                                <Modal
+                                    visible={this.state.modalVisible}
+                                    animationType={'slide'}
+                                    onRequestClose={() => this.closeModal()}>
+
+                                    <View style={styles.modalContainer}>
+                                        <View style={styles.innerContainer}>
+
+                                            <TouchableOpacity onPress={() => this.sendSMS()}>
+                                                <Image source={require('../../images/sms.png')} />
+                                            </TouchableOpacity>
+
+                                            <TouchableOpacity
+                                                onPress={() => this.sendEmail()}>
+                                                <Image source={require('../../images/email.png')} />
+                                            </TouchableOpacity>
+
+                                            <TouchableOpacity onPress={() => this.closeModal()} >
+                                                <Image source={require('../../images/cancel.png')} />
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                </Modal>
 
 
-                        <TouchableOpacity style={styles.buttonContainer} onPress={() => this.onPressNewTrip()}>
-                            <Text style={styles.buttonText}>CREATE TRIP</Text>
-                        </TouchableOpacity>
+                                <TouchableOpacity style={styles.buttonContainer} onPress={() => this.onPressNewTrip()}>
+                                    <Text style={styles.buttonText}>CREATE TRIP</Text>
+                                </TouchableOpacity>
 
+                            </View>
+                        </TouchableWithoutFeedback>
                     </View>
-                </TouchableWithoutFeedback>
-            </View>
-
-        );
-    }
-}
-
+                 
+                    );
+                }
+            }
+            
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
+                        container: {
+                        flex: 1,
+                    backgroundColor: '#fff',
+                },
     tripNameContainer: {
-        flex: 1,
-    },
+                        flex: 1,
+                },
     textHeader: {
-        color: '#000eff'
-    },
+                        color: '#000eff'
+                },
     durationContainer: {
-        flex: 2
-    },
+                        flex: 2
+                },
     startLocationContainer: {
-        flex: 1
-    },
+                        flex: 1
+                },
     endLocationContainer: {
-        flex: 1
-    },
+                        flex: 1
+                },
     addFuncContainer: {
-        flex: 1
-    },
+                        flex: 1
+                },
     container2: {
-        paddingTop: 10
-    },
+                        paddingTop: 10
+                },
     input: {
-        height: 50,
-        width: 350,
-        backgroundColor: 'rgba(255, 255, 255, 0.7)',
-        marginBottom: 10,
-        paddingHorizontal: 10
-    },
+                        height: 50,
+                    width: 350,
+                    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                    marginBottom: 10,
+                    paddingHorizontal: 10
+                },
     durationInput: {
-        height: 50,
-        width: 150,
-        backgroundColor: 'rgba(255, 255, 255, 0.7)',
-        marginBottom: 20,
-        paddingHorizontal: 10,
-        flexDirection: 'row'
-    },
+                        height: 50,
+                    width: 150,
+                    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                    marginBottom: 20,
+                    paddingHorizontal: 10,
+                    flexDirection: 'row'
+                },
     buttonContainer: {
-        backgroundColor: 'rgb(0,25,88)',
-        paddingVertical: 15
-    },
+                        backgroundColor: 'rgb(0,25,88)',
+                    paddingVertical: 15
+                },
     splitContainer: {
-        marginBottom: 20,
-    },
+                        marginBottom: 20,
+                },
     buttonText: {
-        textAlign: 'center',
-        color: '#FFFFFF',
-        fontWeight: 'bold'
-    },
+                        textAlign: 'center',
+                    color: '#FFFFFF',
+                    fontWeight: 'bold'
+                },
     splitText: {
-        textAlign: 'left',
-        color: 'rgb(0,25,88)',
-        fontWeight: 'bold',
-        fontSize: 20,
-    },
+                        textAlign: 'left',
+                    color: 'rgb(0,25,88)',
+                    fontWeight: 'bold',
+                    fontSize: 20,
+                },
     modalContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        height: 300,
-        width: 375,
-    },
+                        flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: 300,
+                    width: 375,
+                },
     innerContainer: {
-        backgroundColor: '#ffffff',
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 10,
-        borderRadius: 4,
-        borderColor: "#ffa53f"
-
-    },
+                        backgroundColor: '#ffffff',
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: 10,
+                    borderRadius: 4,
+                    borderColor: "#ffa53f"
+            
+                },
     newTitleContainer: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'stretch'
-    },
+                        flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'flex-start',
+                    alignItems: 'stretch'
+                },
     newEInput: {
-        //width: '100%',
-        height: 50,
-        //alignItems: 'stretch',
-        justifyContent: 'space-between',
-        backgroundColor: 'white',
-        // borderRadius: 10
-    },
+                        //width: '100%',
+                        height: 50,
+                    //alignItems: 'stretch',
+                    justifyContent: 'space-between',
+                    backgroundColor: 'white',
+                    // borderRadius: 10
+                },
     newEContainer: {
-        flex: 1,
-        //width:
-    }
+                        flex: 1,
+                    //width:
+                }
 });
