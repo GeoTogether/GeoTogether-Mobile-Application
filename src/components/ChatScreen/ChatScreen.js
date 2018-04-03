7f5998e634f1854652b541c1522c4b92373c3316
Benjamin <buriart2@asu.edu>
2018-04-02 18:42:16 -0700

    Began to work on adding picture sending functionality

--- src/components/ChatScreen/ChatScreen.js
+++ src/components/ChatScreen/ChatScreen.js
@@ -1,12 +1,11 @@
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Actions } from 'react-native-gifted-chat';
 import React from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 import firebase from '../Firebase/firebaseStorage';
+import CustomActions from '../Custom/CustomActions';
+
 
 export default class ChatScreen extends React.Component {
-
-
-    
     static navigationOptions = {
         title: 'ChatScreen',
         header: null
@@ -16,80 +15,207 @@ export default class ChatScreen extends React.Component {
         super(props);
         this.state = {messages: []};
         this.onSend = this.onSend.bind(this);
-        this.grabMessages();
+        this._images = [];
+        this.renderCustomActions = this.renderCustomActions.bind(this);
+    }
 
-    }chat
-
-    state = {
-        tripDatabase: "",
-        initialMessages:  [],
-        initialUsers:[],
-        initialTimeStamps:[],
-
-    };
-    
+    stat = {
+        initialTimeStamps: [],
+        firstTime: 0,
+        arrayVal: 0,
+    }
 
 
-    async grabMessages(){
+  componentDidMount(){
+
+    const {state} = this.props.navigation;   
+    var Path = 'trips/' + state.params.tripKey.key + '/chats/groupChat/messages/';
+    var Path2 = 'trips/' + state.params.tripKey.key + '/chats/groupChat/number/';
+
+
+    firebase.database().ref(Path).on('value', (snapshot) => {
        
-    var Path = 'trips/' + state.params.tripKey.key + '/chats/groupChat/messages/{id}';
 
-       
         
 
-        const {state} = this.props.navigation;
-        
-        
-          // Now simply find the parent and return the name.
-
-
-        var TotalArray;
-        firebase.database().ref(Path).on('value', (snapshot) => {
-
-            TotalArray = snapshot.val();
+        if(this.stat.firstTime == 0){
             
-        })
+            var q;
 
-        
+            firebase.database().ref(Path2).once('value', (snapshot) => {q = snapshot.val();});
 
-        var MessageArray =[];
-        var UserArray=[];
-        var TimeStampArray=[];
-        
+            this.stat.arrayVal = q;
 
-        for (var key in TotalArray) {
-            UserArray.push(TotalArray[key][0]);
-            MessageArray.push(TotalArray[key][1]);    
-            TimeStampArray.push(TotalArray[key][2]);
+            this.stat.firstTime = 1;
+       var TotalArray = snapshot.val();
+       var x = 1;
+       var Messages = [];
 
-            
+
+           for (var key in TotalArray) {
+            for (var key2 in TotalArray[key]) {
+                if(x > this.stat.initialTimeStamps.length){                    
+                    this.stat.initialTimeStamps.push(TotalArray[key][key2][2]);
+                        if(TotalArray[key][key2][0] == state.params.email){
+                            Messages.push({
+                                _id: key,
+                                text: TotalArray[key][key2][1],
+                                createdAt: TotalArray[key][key2][2],
+                                user: {
+                                    _id: 1,
+                                    name: TotalArray[key][key2][0],
+                                    avatar: 'https://i.pinimg.com/originals/9c/53/50/9c5350210821ef961feca8e70ebd4160.jpg',
+                                },
+                            });
+                        }
+                        else{
+                            Messages.push({
+                                _id: key,
+                                text: TotalArray[key][key2][1],
+                                createdAt: TotalArray[key][key2][2],
+                                user: {
+                                    _id: key+1,
+                                    name: TotalArray[key][key2][0],
+                                    avatar: 'https://i.pinimg.com/originals/9c/53/50/9c5350210821ef961feca8e70ebd4160.jpg',
+                                },
+                            });
+                        }
+
+                }
+                else{
+
+                }
+               
+                x = x+1;                
+               }
         }
 
-        console.log(MessageArray);
+        this.AddToGui(Messages.reverse());
+    }
 
 
-        /*
-        this.setState({initialMessages: MessageArray});
-        this.setState({initialUsers: UserArray});
-        this.setState({initialTimeStamps: TimeStampArray});
-        */
+    else{
+
+
+                    
+
+                
+
+
+         var q;
+
+        firebase.database().ref(Path2).once('value', (snapshot) => {q = snapshot.val();});
+
+        var temp = this.stat.arrayVal;
+
+        this.stat.arrayVal = temp + 1;
+
+        var Path3 = 'trips/' + state.params.tripKey.key + '/chats/groupChat/messages/' + q;
+
+        var snap;
+        firebase.database().ref(Path3).once('value', (snapshot) => {snap = snapshot.val();});
+
+        var Messages = [];
+
+        console.log(q);
+
+        for(var key in snap){
+        
+            if(snap[key][0] == state.params.email){
+                            Messages.push({
+                                _id: q,
+                                text: snap[key][1],
+                                createdAt: snap[key][2],
+                                user: {
+                                    _id: 1,
+                                    name: snap[key][0],
+                                    avatar: 'https://i.pinimg.com/originals/9c/53/50/9c5350210821ef961feca8e70ebd4160.jpg',
+                                },
+                            });
+                        }
+                        else{
+                            Messages.push({
+                                _id: q,
+                                text: snap[key][1],
+                                createdAt: snap[key][2],
+                                user: {
+                                    _id: q+1,
+                                    name: snap[key][0],
+                                    avatar: 'https://i.pinimg.com/originals/9c/53/50/9c5350210821ef961feca8e70ebd4160.jpg',
+                                },
+                            });
+                        }
+
+
+        }
+
+
+        this.AddToGui(Messages.reverse());
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+    });
+    
+  }
+
+
+    AddToGui(messages = []){
+        this.setState((previousState) => {
+                return {
+                    messages: GiftedChat.append(previousState.messages, messages),
+                };
+        });
+
+    }
+
     sendMessage(message){
+
         const {state} = this.props.navigation;
+
         
-        var Path = 'trips/' + state.params.tripKey.key + '/chats/groupChat/messages/';
+        var Path2 = 'trips/' + state.params.tripKey.key + '/chats/groupChat/number/';
+
+        var q;
+
+        firebase.database().ref(Path2).once('value', (snapshot) => {q = snapshot.val();});
+
+        firebase.database().ref(Path2).set((q+1));
+
+
+        var Path = 'trips/' + state.params.tripKey.key + '/chats/groupChat/messages/' + (q+1);
+
+
+
 
         firebase.database().ref(Path).push({
             
                     
                     0: state.params.email,
                     1: message,
-                    2: this.getCurrentTime(),
-                    
-                
+                    2: this.getCurrentTime(),     
             
         });
     }
@@ -112,39 +238,47 @@ export default class ChatScreen extends React.Component {
         var currentdate = new Date(); 
         return currentdate.getTime();
     }
-    
+
 
     componentWillMount() {
-       
-         const {state} = this.props.navigation;
-
-        this.setState({
-            messages: [
-                {
-                    _id: 1,
-                    text: state.params.email,
-                    createdAt: 1521622981696,
-                    user: {
-                        _id: 2,
-                        name: 'React Native',
-                        avatar: 'https://i.pinimg.com/originals/9c/53/50/9c5350210821ef961feca8e70ebd4160.jpg',
-                    },
-                },
-            ],
-        });
+         
+        
+        
     }
     onSend(messages = []) {
         this.sendMessage(messages[0].text);
+    }
 
-        this.setState((previousState) => {
-            return {
-                messages: GiftedChat.append(previousState.messages, messages),
-            };
-        });
+    renderCustomActions(props) {
+        if (Platform.OS === 'ios') {
+            return (
+                <CustomActions
+                    {...props}
+                />
+            );
+        }
+        const options = {
+            'Action 1': (props) => {
+                alert('option 1');
+            },
+            'Action 2': (props) => {
+                alert('option 2');
+            },
+            'Cancel': () => {},
+        };
+        return (
+            <Actions
+                {...props}
+                options={options}
+            />
+        );
     }
 
 
+
     render() {
+        const {state} = this.props.navigation;
+        var email = state.params.email;
         return (
 
             <GiftedChat
@@ -152,8 +286,9 @@ export default class ChatScreen extends React.Component {
                 onSend={this.onSend}
                 user={{
                     _id: 1,
-                    name: 'Benji',
+                    name: email,
                 }}
+                renderActions={this.renderCustomActions}
             />
 
         );
